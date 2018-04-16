7532a0758f15f5f77f68bfe5a34e8796e39e88cf
noob <jumpwin613@naver.com>
2018-04-16 22:03:23 +0900

    E-Hen Test

---
+++ Hitomi Copy 2/GalleryInfo/HitomiGalleryInfoModel.cs
@@ -0,0 +1,16 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developers */
+
+using Newtonsoft.Json;
+
+namespace Hitomi_Copy_2.GalleryInfo
+{
+    public class HitomiGalleryInfoModel
+    {
+        [JsonProperty]
+        public int width;
+        [JsonProperty]
+        public string name;
+        [JsonProperty]
+        public int height;
+    }
+}
---
+++ Hitomi Copy 2/Hitomi 403/ehen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hitomi_403
+{
+    public partial class ehen : Form
+    {
+        public ehen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // https://e-hentai.org/?page=1
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            MessageBox.Show(wc.DownloadString(new Uri($"https://e-hentai.org/?page=1")));
+        }
+    }
+}
