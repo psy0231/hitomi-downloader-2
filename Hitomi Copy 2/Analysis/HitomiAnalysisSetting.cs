75d8fd052b72f11afb163cb740da99d57675c3af
noob <jumpwin613@naver.com>
2018-04-16 09:49:14 +0900

    Tidy
    Tidy All

--- Hitomi Copy 2/Analysis/HitomiAnalysisSetting.cs
+++
@@ -1,9 +0,0 @@
-﻿/* Copyright (C) 2018. Hitomi Parser Developers */
-
-namespace Hitomi_Copy_2.Analysis
-{
-    public class HitomiAnalysisSetting
-    {
-        public static bool MajorityTags = false;
-    }
-}
