6cfd8a44898c05f55a5546ee719487aae929741e
noob <jumpwin613@naver.com>
2018-04-19 23:59:01 +0900

    Pixiv Downloader Prototype

---
+++ Pixiv Downloader/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pixiv_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            string html =  wc.DownloadString(new Uri("https://www.pixiv.net/member.php?id=3189901"));
+
+            PX.PixivParser.GetImagesAddress(html);
+        }
+    }
+}
---
+++ Pixiv Downloader/PX/PixivParser.cs
@@ -0,0 +1,20 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developer */
+
+using HtmlAgilityPack;
+
+namespace Pixiv_Downloader.PX
+{
+    public class PixivParser
+    {
+        public static string GetImagesAddress(string source)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(source);
+            HtmlNode nodes = document.DocumentNode.SelectNodes("//div[@id='wrapper']")[0];
+
+            HtmlNodeCollection items = nodes.SelectNodes(".//div[@class='layout-a']");
+
+            return nodes.SelectSingleNode(".//div[@id='i3']//a//img").GetAttributeValue("src", "");
+        }
+    }
+}
