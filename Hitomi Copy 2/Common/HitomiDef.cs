83fd4d8e811e193290061d624d2e014b48175b23
noob <jumpwin613@naver.com>
2018-04-21 09:44:49 +0900

    Fix GetDownloadImageAddress
    https://ltn.hitomi.la/common.js

--- Hitomi Copy 2/Common/HitomiDef.cs
+++ Hitomi Copy 2/Common/HitomiDef.cs
@@ -22,7 +22,7 @@ namespace hitomi.Parser
             // download.js
             var number_of_frontends = 2;
             char subdomain = Convert.ToChar(97 + (Convert.ToInt32(gallery.Last()) % number_of_frontends));
-            if (gallery.Last() == '1' || gallery.Last() == '3')
+            if (gallery.Last() == '1')
                 subdomain = 'a';
             return $"https://{subdomain}a.hitomi.la/galleries/{gallery}/{page_with_extension}";
         }
