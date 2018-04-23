d38ab4807e3030fe19b49c052a183b01d0101342
noob <jumpwin613@naver.com>
2018-04-23 23:10:31 +0900

    [Twitter] Error get tweet with tweetinvi.

--- Twitter Downloader/Form1.cs
+++ Twitter Downloader/Form1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 
 namespace Twitter_Downloader
 {
@@ -15,6 +16,42 @@ namespace Twitter_Downloader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // https://twitter.com/otonajyojifuku/media
+            
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackAndAwait;
+
+            RateLimit.QueryAwaitingForRateLimit += (s, args) =>
+            {
+                Console.WriteLine($"Query : {args.Query} is awaiting for rate limits!");
+            };
+
+            long userId = 1706386254;
+            var lastTweets = Timeline.GetUserTimeline(userId, 200).ToArray();
+
+            var allTweets = new List<ITweet>(lastTweets);
+            var beforeLast = allTweets;
+
+            while (lastTweets.Length > 0 && allTweets.Count <= 3200)
+            {
+                var idOfOldestTweet = lastTweets.Select(x => x.Id).Min();
+                Console.WriteLine($"Oldest Tweet Id = {idOfOldestTweet}");
+
+                var numberOfTweetsToRetrieve = allTweets.Count > 3000 ? 3200 - allTweets.Count : 200;
+                var timelineRequestParameters = new UserTimelineParameters
+                {
+                    // MaxId ensures that we only get tweets that have been posted 
+                    // BEFORE the oldest tweet we received
+                    MaxId = idOfOldestTweet - 1,
+                    MaximumNumberOfTweetsToRetrieve = numberOfTweetsToRetrieve
+                };
+
+                lastTweets = Timeline.GetUserTimeline(userId, timelineRequestParameters).ToArray();
+                allTweets.AddRange(lastTweets);
+            }
         }
     }
 }
