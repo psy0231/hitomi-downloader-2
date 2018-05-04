3f57b01bca72a5332cdc2e4c1a4e75ea2c41bd5d
dc-koromo <jumpwin613@naver.com>
2018-05-05 00:31:29 +0900

    [Hitomi 3] Language Prototype

---
+++ Hitomi Copy 3/Language.cs
@@ -0,0 +1,33 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developers */
+
+namespace Hitomi_Copy_3
+{
+    interface ILanguage
+    {
+        string TabSearch();
+        string TabDownload();
+        string TabArtistRecommend();
+        string TabSetting();
+
+        string TabSearchButtonSearch();
+        string TabSearchButtonTidy();
+        string TabSearchButtonSelectAll();
+        string TabSearchButtonDeSelectAll();
+        string TabSearchButtonDownload();
+        string TabSearchMessageSearched();
+
+        string TabDownloadMessageStayedElement();
+        string TabDownloadMessageDownloadState();
+    }
+
+    //public class KoreanLanguage : ILanguage
+    //{
+    //    string ILanguage.TabSearch() => "Tab";
+
+    //}
+
+    public class Test
+    {
+    }
+
+}
