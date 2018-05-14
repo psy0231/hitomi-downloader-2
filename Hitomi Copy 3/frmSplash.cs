de0a4ed760e11e259b789f4c4949ee53fb625eda
dc-koromo <jumpwin613@naver.com>
2018-05-14 09:47:24 +0900

    [Hitomi 3] Splash error message

--- Hitomi Copy 3/frmSplash.cs
+++ Hitomi Copy 3/frmSplash.cs
@@ -2,6 +2,7 @@
 
 using Hitomi_Copy.Data;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,29 +17,38 @@ namespace Hitomi_Copy_2
 
         private async void frmSplash_Load(object sender, EventArgs e)
         {
-            if (HitomiData.Instance.CheckTagdataExist())
+            try
             {
-                await Task.Run(() => HitomiData.Instance.LoadTagdataJson());
-            }
-            else
-            {
-                lStatus.Text = "데이터를 다운로드 중 입니다... 이 작업은 수 분 정도 걸립니다.";
-                await Task.Run(() => HitomiData.Instance.DownloadTagdata());
-            }
+                if (HitomiData.Instance.CheckTagdataExist())
+                {
+                    await Task.Run(() => HitomiData.Instance.LoadTagdataJson());
+                }
+                else
+                {
+                    lStatus.Text = "데이터를 다운로드 중 입니다... 이 작업은 수 분 정도 걸립니다.";
+                    await Task.Run(() => HitomiData.Instance.DownloadTagdata());
+                }
 
-            if (HitomiData.Instance.CheckMetadataExist())
-            {
-                await Task.Run(() => HitomiData.Instance.LoadMetadataJson());
+                if (HitomiData.Instance.CheckMetadataExist())
+                {
+                    await Task.Run(() => HitomiData.Instance.LoadMetadataJson());
+                }
+                else
+                {
+                    lStatus.Text = "데이터를 다운로드 중 입니다... 이 작업은 수 분 정도 걸립니다.";
+                    await Task.Run(() => HitomiData.Instance.DownloadMetadata());
+                }
+
+                (Application.OpenForms["frmMain"] as Hitomi_Copy_3.frmMain).OnTab();
+                Application.OpenForms["frmMain"].BringToFront();
+                this.Close();
             }
-            else
+            catch (Exception ex)
             {
-                lStatus.Text = "데이터를 다운로드 중 입니다... 이 작업은 수 분 정도 걸립니다.";
-                await Task.Run(() => HitomiData.Instance.DownloadMetadata());
+                MessageBox.Show("심각한 오류가 발생하여 계속 진행할 수 없습니다. 네트워크 설정이 잘못되었거나 차단되어있을 가능성이 높습니다." +
+                    "이 오류가 계속 발생한다면 koromo.software@gmail.com로 오류메시지를 보내주세요.\r\n메시지: "+ex.Message+"\r\n스택: "+ex.StackTrace, "Hitomi Copy", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
             }
-            
-            (Application.OpenForms["frmMain"] as Hitomi_Copy_3.frmMain).OnTab();
-            Application.OpenForms["frmMain"].BringToFront();
-            this.Close();
         }
     }
 }
