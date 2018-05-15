282136b1f4d8eb9efa37e20bb3c7746d07ba1e43
dc-koromo <jumpwin613@naver.com>
2018-05-15 11:55:39 +0900

    [Hitomi 3] Add statrate prototype.

--- Hitomi Copy 3/frmStatRate.Designer.cs
+++ Hitomi Copy 3/frmStatRate.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.clbTags = new System.Windows.Forms.CheckedListBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clbOpposite = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // clbTags
@@ -50,12 +56,72 @@
             this.label2.TabIndex = 12;
             this.label2.Text = "태그 : ";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 15F);
+            this.label1.Location = new System.Drawing.Point(292, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 28);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "대립군 : ";
+            // 
+            // clbOpposite
+            // 
+            this.clbOpposite.FormattingEnabled = true;
+            this.clbOpposite.Location = new System.Drawing.Point(297, 40);
+            this.clbOpposite.Name = "clbOpposite";
+            this.clbOpposite.Size = new System.Drawing.Size(470, 166);
+            this.clbOpposite.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 15F);
+            this.label3.Location = new System.Drawing.Point(292, 405);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 28);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "최종 보정 값 : ";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(297, 436);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(470, 148);
+            this.checkedListBox1.TabIndex = 16;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(297, 240);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(470, 166);
+            this.checkedListBox2.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 15F);
+            this.label4.Location = new System.Drawing.Point(292, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 28);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "주요 태그 : ";
+            // 
             // frmStatRate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1036, 600);
+            this.ClientSize = new System.Drawing.Size(788, 600);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clbOpposite);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.clbTags);
             this.Font = new System.Drawing.Font("맑은 고딕", 9F);
@@ -73,5 +139,11 @@
 
         private System.Windows.Forms.CheckedListBox clbTags;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox clbOpposite;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label4;
     }
 }
