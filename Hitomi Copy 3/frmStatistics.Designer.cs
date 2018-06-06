269f3614396e3a82943abdd8169051ce95603539
dc-koromo <jumpwin613@naver.com>
2018-06-06 16:08:46 +0900

    [Hitomi 3] Add statistics prototype

---
+++ Hitomi Copy 3/frmStatistics.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Hitomi_Copy_3
+{
+    partial class frmStatistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 454);
+            this.Name = "frmStatistics";
+            this.Text = "frmStatistics";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Hitomi Copy 3/frmStatistics.cs
@@ -0,0 +1,12 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developers */
+
+namespace Hitomi_Copy_3
+{
+    public partial class frmStatistics : MetroFramework.Forms.MetroForm
+    {
+        public frmStatistics()
+        {
+            InitializeComponent();
+        }
+    }
+}
