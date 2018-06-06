4149b0ab5c34dac11ecb5e5d8d2cb41e1df6e337
dc-koromo <jumpwin613@naver.com>
2018-06-06 20:29:26 +0900

    [Hitomi 3] Tidy namespace

--- Hitomi Copy 3/frmStatistics.cs
+++ Hitomi Copy 3/frmStatistics.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
