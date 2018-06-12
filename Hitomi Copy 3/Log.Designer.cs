e87e16e223fce796a8f75a771b9f1a0809b2785f
dc-koromo <jumpwin613@naver.com>
2018-06-12 18:09:35 +0900

    [Hitomi 3] Modify 'Disable Logging' anchor

--- Hitomi Copy 3/Log.Designer.cs
+++ Hitomi Copy 3/Log.Designer.cs
@@ -61,7 +61,7 @@
             // 
             // button2
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button2.Location = new System.Drawing.Point(12, 702);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(129, 24);
