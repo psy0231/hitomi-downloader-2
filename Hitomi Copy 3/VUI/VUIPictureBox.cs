c1ac856f15772acaf950bb963dd9c4c51a589e11
dc-koromo <jumpwin613@naver.com>
2018-06-24 13:55:54 +0900

    [Hitomi 3] Add VUI PictureBox

--- Hitomi Copy 3/VUI/VUIPictureBox.cs
+++ Hitomi Copy 3/VUI/VUIPictureBox.cs
@@ -1,6 +1,7 @@
 ﻿/* Copyright (C) 2018. Hitomi Parser Developers */
 
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace Hitomi_Copy_3.VUI
 {
@@ -10,7 +11,14 @@ namespace Hitomi_Copy_3.VUI
 
         public override void Paint(Graphics g)
         {
-
+            PictureBox pict = new PictureBox();
+            pict.Size = Size;
+            pict.SizeMode = PictureBoxSizeMode.Zoom;
+            var bm = new Bitmap(pict.ClientSize.Width, pict.ClientSize.Height);
+            pict.Image = Image;
+            pict.DrawToBitmap(bm, pict.ClientRectangle);
+            g.DrawImage(bm, pict.Bounds);
+            pict.Dispose();
         }
     }
 }
