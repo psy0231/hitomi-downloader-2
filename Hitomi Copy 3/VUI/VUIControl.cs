ed37c7c715c1942d00b115333eec07231443310c
dc-koromo <jumpwin613@naver.com>
2018-06-24 13:51:31 +0900

    [Hitomi 3] Add VUI Control Interface

---
+++ Hitomi Copy 3/VUI/VUIControl.cs
@@ -0,0 +1,28 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developers */
+
+using System.Drawing;
+
+namespace Hitomi_Copy_3.VUI
+{
+    public abstract class VUIControl
+    {
+        public Point Location { get; set; }
+        public Size Size { get; set; }
+        public delegate void Action();
+        public Action ClickEvent;
+
+        public Action MouseEnterEvent;
+        public Action MouseMoveEvent;
+        public Action MouseLeaveEvent;
+
+        public abstract void Paint(Graphics g);
+
+        public bool Intersect(Point P)
+        {
+            int sx = P.X - Location.X;
+            int sy = P.Y - Location.Y;
+            if (Size.Height >= sy && Size.Width >= sx) return true;
+            return false;
+        }
+    }
+}
--- Hitomi Copy 3/VUI/VUIPictureBox.cs
+++ Hitomi Copy 3/VUI/VUIPictureBox.cs
@@ -4,14 +4,13 @@ using System.Drawing;
 
 namespace Hitomi_Copy_3.VUI
 {
-    public class VUILabel : VUIControl
+    public class VUIPictureBox : VUIControl
     {
-        public Font Font { get; set; }
-        public string Text { get; set; }
+        public Image Image { get; set; }
 
         public override void Paint(Graphics g)
         {
-            g.DrawString(Text, Font, Brushes.Black, Location);
+
         }
     }
 }
